01f5cd58650a7fbdca4950ae3780bcd7e4ad8408
WesternRanger <western_ranger@163.com>
2014-12-02 21:36:21 +0800

    改成面向对象

--- js/drag.js
+++ js/drag.js
@@ -1,75 +1,24 @@
 /**
  * Created by 陈奇 on 14-10-9.
  */
-function drag(p){
-	var mouseOffsetX = 0;
-	var mouseOffsetY = 0;
-	var isDraging = false;
+
+function Drag(p){
+	var self = this;
+	this.title = p.title;//鼠标要拖拽的标题栏
+	this.box = p.box;//被拖动体
+	this.mask = p.mask;//半透明遮罩层
+	this.open = p.open;//调出浮层的触发按钮
+	this.close = p.close;//关闭浮层的按钮
+
+	this.mouseOffsetX = 0;
+	this.mouseOffsetY = 0;
+	this.isDraging = false;
 	//获取dom元素
-	function g(id){
+	this.g = function(id){
 		return document.getElementById(id);
 	}
-//鼠标事件一，在标题栏按下，（要计算鼠标相对于拖拽元素的左上角的坐标，并且标记元素为可拖动）
-	g(p.title).addEventListener('mousedown',function(e){
-		var e = e || window.event;
-		mouseOffsetX = e.pageX - g(p.box).offsetLeft;
-		mouseOffsetY = e.pageY - g(p.box).offsetTop;
-		isDraging = true;
-	},false);
-//鼠标事件二，鼠标移动（要检测鼠标是否标记为移动，如果是，则更新元素的位置到当前鼠标的位置，即，要减去第一步中获得的偏移）
-	document.onmousemove = function(e){
-		var e = e || window.event;
-		var mouseX = e.pageX,mouseY = e.pageY,moveX = 0,moveY = 0;
-		if(isDraging){
-			moveX = mouseX - mouseOffsetX;
-			moveY = mouseY - mouseOffsetY;
-
-			//范围限定
-			var pageWidth = document.documentElement.clientWidth;
-			var pageHeight = document.documentElement.clientHeight;
-
-			var dialogWidth = g(p.box).offsetWidth;
-			var dialogHeight = g(p.box).offsetHeight;
-
-			var maxX = pageWidth - dialogWidth;
-			var maxY = pageHeight - dialogHeight;
-
-			moveX = Math.min( maxX,Math.max(0,moveX));
-			moveY = Math.min( maxY,Math.max(0,moveY));
-
-			g(p.box).style.left = moveX + 'px';
-			g(p.box).style.top = moveY + 'px';
-		}
-	}
-//鼠标事件三，鼠标松开的时候（标记元素为不可拖动即可）
-	document.onmouseup = function(){
-		isDraging = false;
-	}
-
-
-
-	//打开浮层
-	function showDialog(){
-		g(p.box).style.display = 'block';
-		g(p.mask).style.display = 'block';
-		fillToBody(g(p.mask));
-		autoCenter(g(p.box));
-	}
-	//关闭浮层
-	function hideDialog(){
-		g(p.box).style.display = 'none';
-		g(p.mask).style.display = 'none';
-	}
-	g(p.open).addEventListener('click',showDialog,false);
-	g(p.close).addEventListener('click',hideDialog,false);
-
-	//窗口大小变化，dialog始终居中
-	window.onresize = function(){
-		fillToBody(g(p.mask));
-		autoCenter(g(p.box));
-	}
 	//自动居中
-	function autoCenter( el ){
+	this.autoCenter = function( el ){
 		var bodyW = document.documentElement.clientWidth;
 		var bodyH = document.documentElement.clientHeight;
 
@@ -80,8 +29,83 @@ function drag(p){
 		el.style.top = (bodyH-elH)/2 + 'px';
 	}
 //遮罩层充满屏幕
-	function fillToBody( el ){
+	this.fillToBody = function( el ){
 		el.style.width = '100%';
 		el.style.height ='100%';
 	}
+	//打开浮层
+	this.showDialog = function(){
+		self.g(self.box).style.display = 'block';
+		self.g(self.mask).style.display = 'block';
+		self.fillToBody(self.g(self.mask));
+		self.autoCenter(self.g(self.box));
+	}
+	//关闭浮层
+	this.hideDialog = function(){
+		self.g(self.box).style.display = 'none';
+		self.g(self.mask).style.display = 'none';
+	}
+//鼠标事件一，在标题栏按下，（要计算鼠标相对于拖拽元素的左上角的坐标，并且标记元素为可拖动）
+	self.g(self.title).onmousedown = function(e){
+		self.mouseDown(e);
+	}
+//鼠标事件二，鼠标移动（要检测鼠标是否标记为移动，如果是，则更新元素的位置到当前鼠标的位置，即，要减去第一步中获得的偏移）
+	document.onmousemove = function(e){
+		self.mouseMove(e);
+	}
+//鼠标事件三，鼠标松开的时候（标记元素为不可拖动即可）
+	document.onmouseup = function(){
+		self.isDraging = false;
+	}
+	self.g(p.open).onclick = function(){
+		self.showDialog();
+	}
+	self.g(p.close).onclick = function(){
+		self.hideDialog();
+	}
+
+	//窗口大小变化，dialog始终居中
+	window.onresize = function(){
+		self.resize();
+	}
+}
+Drag.prototype.mouseDown = function(e){
+	var e = e || window.event;
+	var self = this;
+
+	this.mouseOffsetX = e.pageX - self.g(self.box).offsetLeft;
+	this.mouseOffsetY = e.pageY - self.g(self.box).offsetTop;
+	this.isDraging = true;
+}
+Drag.prototype.mouseMove = function(e){
+	var e = e || window.event;
+	var self = this;
+
+	var mouseX = e.pageX,mouseY = e.pageY,moveX = 0,moveY = 0;
+	if(this.isDraging){
+		moveX = mouseX - this.mouseOffsetX;
+		moveY = mouseY - this.mouseOffsetY;
+
+		//范围限定
+		var pageWidth = document.documentElement.clientWidth;
+		var pageHeight = document.documentElement.clientHeight;
+
+		var dialogWidth = self.g(self.box).offsetWidth;
+		var dialogHeight = self.g(self.box).offsetHeight;
+
+		var maxX = pageWidth - dialogWidth;
+		var maxY = pageHeight - dialogHeight;
+
+		moveX = Math.min( maxX,Math.max(0,moveX));
+		moveY = Math.min( maxY,Math.max(0,moveY));
+
+		self.g(self.box).style.left = moveX + 'px';
+		self.g(self.box).style.top = moveY + 'px';
+	}
+}
+Drag.prototype.resize = function(){
+	var self = this;
+
+	self.fillToBody(self.g(self.mask));
+	self.autoCenter(self.g(self.box));
 }
